5365db3c78d73a19002530892d80159b81a2e630
Jordan Morse <jtm6496@gmail.com>
2020-12-17 21:01:54 -0500

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("public"))
+
+const URL = process.env.MONGODBURL || "mongodb://localhost/workout"
+
+mongoose.connect(URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+  })
+
+app.listen(PORT, () => {console.log("app running queen!")})
